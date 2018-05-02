0b71f5943e1de9d5be119a7a95d1c53f52c0947f
Jean-Marc Prieur <jmprieur@microsoft.com>
2018-05-02 08:43:48 +0200

    Archiving the sample

--- app/scripts/userDataCtrl.js
+++
@@ -1,5 +0,0 @@
-﻿'use strict';
-angular.module('todoApp')
-
-// Load ADAL for use in view
-.controller('userDataCtrl', ['$scope', 'adalAuthenticationService', function ($scope, adalService) {}]);
--- config.js
+++
@@ -1,8 +0,0 @@
- exports.creds = {
-     
-     // The app id you get from the registration portal
-     audience: '<Your-application-id>',
-     
-     // Passport will use this URL to fetch the token validation information from Azure AD
-     identityMetadata: 'https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration'
- };
